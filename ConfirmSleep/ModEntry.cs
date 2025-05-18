6d0c1815d81823c8e6de0d5464c9f20949ffadee
emurphy42 <emurphy42@gmail.com>
2025-05-17 22:13:49 -0700

    Add project files.

---
+++ ConfirmSleep/ModEntry.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+
+namespace ConfirmSleep
+{
+    public class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            ObjectPatches.ModInstance = this;
+
+            var harmony = new Harmony(this.ModManifest.UniqueID);
+            harmony.Patch(
+                original: AccessTools.Method(typeof(StardewValley.Game1), nameof(StardewValley.Game1.showEndOfNightStuff)),
+                prefix: new HarmonyMethod(typeof(ObjectPatches), nameof(ObjectPatches.Game1_showEndOfNightStuff_Prefix))
+            );
+        }
+    }
+}
---
+++ ConfirmSleep/ObjectPatches.cs
@@ -0,0 +1,35 @@
+﻿using ConfirmSleep;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using static StardewValley.GameLocation;
+
+namespace ConfirmSleep
+{
+    internal class ObjectPatches
+    {
+        // initialized by ModEntry.cs
+        public static ModEntry ModInstance;
+        
+        public static bool Game1_showEndOfNightStuff_Prefix()
+        {
+            // if needed, force player to see a shipped-items list (which they must then confirm before day ends)
+            // Spring 1 Year 1: DaysPlayed = 1 at start of day, 2 at end of day
+            if (Game1.player.displayedShippedItems.Count == 0 && Game1.stats.DaysPlayed > 1)
+            {
+                ModInstance.Monitor.Log("[Confirm Sleep] Shipped-items list is empty", LogLevel.Trace);
+                var obj = ItemRegistry.Create("JohnPeters.NoTea");
+                ModInstance.Monitor.Log("[Confirm Sleep] Created placeholder item", LogLevel.Trace);
+                Game1.player.displayedShippedItems.Add(obj);
+                ModInstance.Monitor.Log("[Confirm Sleep] Added placeholder item to shipped-items list", LogLevel.Debug);
+            }
+
+            // allow base game function to run normally
+            return true;
+        }
+    }
+}
